5bcae3eb36fb68e62dd9a031a153f23f37f26835
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-03-12 23:13:56 +0900

    [FEAT] #67 - 목표생성 결과화면 디자인 반영

--- dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/SetGoal/GoalResultView.swift
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/SetGoal/GoalResultView.swift
@@ -12,11 +12,66 @@ struct GoalResultView: View {
     let store: StoreOf<GoalResult>
     var body: some View {
         WithPerceptionTracking {
-            VStack {
-                Text(store.goalTitle)
-                    .headingStyle2()
-                Text(store.planTitle)
-                    .headingStyle2()
+            VStack(spacing: 0) {
+                Text("목표와 할 일을 \n생성했어요")
+                    .headingStyle1()
+                    .alignmentLeading()
+                    .foregroundStyle(.gray900)
+                    .padding(.top, 40)
+                Text("작은 일을 하나씩 완료하며 목표로 나아가요")
+                    .bodyLargeMedium()
+                    .alignmentLeading()
+                    .foregroundStyle(.gray800)
+                    .padding(.top, 8)
+                
+                Text("목표")
+                    .bodyXLargeSemibold()
+                    .alignmentLeading()
+                    .foregroundStyle(.gray900)
+                    .padding(.top, 48)
+                HStack {
+                    Text(store.goalTitle)
+                        .bodyMediumMedium()
+                        .foregroundStyle(.purple900)
+                        .padding(12)
+                        .background(.purple50)
+                        .cornerRadius(8)
+                        .padding(.top, 4)
+                    Spacer()
+                }
+                
+                Text("할 일")
+                    .bodyXLargeSemibold()
+                    .alignmentLeading()
+                    .foregroundStyle(.gray900)
+                    .padding(.top, 20)
+                HStack {
+                    Text(store.planTitle)
+                        .bodyMediumMedium()
+                        .foregroundStyle(.purple900)
+                        .padding(12)
+                        .background(.purple50)
+                        .cornerRadius(8)
+                        .padding(.top, 4)
+                    Spacer()
+                }
+                
+                Text("시간")
+                    .bodyXLargeSemibold()
+                    .alignmentLeading()
+                    .foregroundStyle(.gray900)
+                    .padding(.top, 20)
+                HStack {
+                    Text(store.dateString)
+                        .bodyMediumMedium()
+                        .foregroundStyle(.purple900)
+                        .padding(12)
+                        .background(.purple50)
+                        .cornerRadius(8)
+                        .padding(.top, 4)
+                    Spacer()
+                }
+                
                 Spacer()
                 
                 DDButton(title: "시작하기") {
