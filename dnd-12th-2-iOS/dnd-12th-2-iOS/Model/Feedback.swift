9048452b21e16b8b3d032aaaae0d53d1e486953d
Allie <wooyani77@gmail.com>
2025-03-12 19:47:19 +0900

    [FEAT] #69 - Feedback Model 추가

---
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Model/Feedback.swift
@@ -0,0 +1,15 @@
+//
+//  Feedback.swift
+//  dnd-12th-2-iOS
+//
+//  Created by Allie on 3/12/25.
+//
+
+import Foundation
+
+struct Feedback: Codable {
+    let question: String
+    let description: String
+    let order: Int
+    let indicators: [String]
+}
