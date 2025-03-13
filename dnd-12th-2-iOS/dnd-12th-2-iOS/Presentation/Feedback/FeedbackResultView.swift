517fa7443734393102763af386ad76d667069f92
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-03-13 11:53:41 +0900

    [FEAT] #72 - 피드백 결과화면 디자인반영

--- dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/Feedback/FeedbackResultView.swift
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/Feedback/FeedbackResultView.swift
@@ -10,13 +10,125 @@ import ComposableArchitecture
 
 struct FeedbackResultView: View {
     let store: StoreOf<FeedbackResult>
+    @Environment(\.safeAreaInsets) private var safeAreaInsets
     var body: some View {
         VStack {
-            
+            ImageBackground()
+            FeedbackReslt()
+            Spacer()
+            ResultButton()
         }
-        .navigationBar(left: {
-            EmptyView()
-        })
+        .ignoresSafeArea(.container, edges: .top)
+        .navigationBarHidden(true)
+    }
+}
+
+extension FeedbackResultView {
+    @ViewBuilder
+    func ImageBackground() -> some View {
+        switch store.planInfo.completeType {
+        case .success:
+            VStack {
+                HStack(spacing: 8) {
+                    Image("successBadge")
+                    VStack {
+                        Text("앞으로의 \n한 걸음도 기대할게요.")
+                            .headingStyle3()
+                    }
+                    Spacer()
+                }
+                .padding(.top, safeAreaInsets.top + 48)
+                .padding(.horizontal, 16)
+                .padding(.vertical, 20)
+                .padding(.bottom, 20)
+            }
+            .frame(maxWidth: .infinity)
+            .background(Color.success.overlay(alignment: .top, content: {
+                Image("feedbackBackground")
+            }))
+            
+        case .failure:
+            VStack {
+                HStack(spacing: 8) {
+                    Image("target")
+                    VStack {
+                        Text("다음에는 지금보다 \n훨씬 더 수월할 거예요.")
+                            .headingStyle3()
+                    }
+                    Spacer()
+                }
+                .padding(.top, safeAreaInsets.top + 48)
+                .padding(.horizontal, 16)
+                .padding(.vertical, 20)
+                .padding(.bottom, 20)
+            }
+            .frame(maxWidth: .infinity)
+            .background(Color.failure)
+        }
+    }
+    
+    @ViewBuilder
+    func FeedbackReslt() -> some View {
+        VStack {
+            VStack(spacing: 0) {
+                Text("나의 계획")
+                    .bodyLargeBold()
+                    .alignmentLeading()
+                    .foregroundStyle(.gray900)
+                    .padding(.top, 24)
+                
+                CompleteList()
+                    .padding(.top, 12)
+                
+                Text("다음에는 이렇게 도달할까요?")
+                    .bodyLargeBold()
+                    .alignmentLeading()
+                    .foregroundStyle(.gray900)
+                    .padding(.top, 24)
+                
+                HStack(spacing: 8) {
+                    Image("iconCheckSmall")
+                    Text("구체적인 계획을 설정해요.")
+                        .font(.pretendard(size: 14, weight: .semibold))
+                        .foregroundStyle(Color.gray800)
+                    Spacer()
+                }
+                .padding(.vertical, 16)
+                .padding(.horizontal, 12)
+                .background(Color.purple100)
+                .cornerRadius(8)
+                .padding(.top, 12)
+                
+            }
+            .padding(.horizontal, 16)
+            Spacer()
+        }
+        .background(.white)
+        .cornerRadius(20)
+        .offset(y: -20)
+    }
+    
+    @ViewBuilder
+    func ResultButton() -> some View {
+        VStack {
+            switch store.planInfo.completeType {
+            case .success:
+                HStack(spacing: 8) {
+                    DDButton(title: "다음에도 할래요", backgroundColor: .purple50, textColor: .purple600) {
+                        
+                    }
+                    
+                    DDButton(title: "확인") {
+                        
+                    }
+                }
+            case .failure:
+                DDButton(title: "계획 재설정하기") {
+                    
+                }
+            }
+        }
+        .padding(.horizontal, 16)
     }
 }
 
