0a9912b1198371aaacae9eae618a0bc5276e1ba9
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-03-19 15:04:18 +0900

    [FEAT] #38 - 마이페이지 디자인 반영

--- dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/MyPage/MyPageView.swift
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/MyPage/MyPageView.swift
@@ -12,19 +12,94 @@ struct MyPageView: View {
     let store: StoreOf<MyPage>
     var body: some View {
         WithPerceptionTracking {
-            VStack {
-                Text("마이페이지")
-                    .font(.title)
+            VStack(spacing: 0) {
+//                Text("마이페이지")
+//                    .font(.title)
+//                Button(action: {
+//                    store.send(.logoutButtonTapped)
+//                }, label: {
+//                    Text("logout")
+//                })
+                
+                Text("이메일")
+                    .bodyXLargeSemibold()
+                    .alignmentLeading()
+                    .foregroundStyle(.gray900)
+                    .padding(.top, 40)
+                Text("email")
+                    .bodyMediumMedium()
+                    .alignmentLeading()
+                    .foregroundStyle(.gray900)
+                    .padding(.top, 12)
+                
                 Button(action: {
-                    store.send(.logoutButtonTapped)
+                    
                 }, label: {
-                    Text("logout")
+                    Text("1:1 문의")
+                        .bodyLargeSemibold()
+                        .alignmentLeading()
+                        .foregroundStyle(.gray900)
+                        .padding(.vertical, 16)
+                        .padding(.horizontal, 12)
                 })
+                .background(.white)
+                .cornerRadius(12)
+                .padding(.top, 48)
+                
+                Button(action: {
+                    store.send(.showLogoutAlert)
+                }, label: {
+                    Text("로그아웃")
+                        .bodyLargeSemibold()
+                        .alignmentLeading()
+                        .foregroundStyle(.red)
+                        .padding(.vertical, 16)
+                        .padding(.horizontal, 12)
+                })
+                .background(.white)
+                .cornerRadius(12)
+                .padding(.top, 16)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("회원탈퇴")
+                        .bodyLargeSemibold()
+                        .alignmentLeading()
+                        .foregroundStyle(.red)
+                        .padding(.vertical, 16)
+                        .padding(.horizontal, 12)
+                })
+                .background(.white)
+                .cornerRadius(12)
+                .padding(.top, 16)
+                
+                Spacer()
             }
-            .navigationBar(left: {
+            .navigationBar(center: {
+                Text("마이페이지")
+                    .bodyLargeBold()
+                    .foregroundStyle(.gray800)
+            }, left: {
                 DDBackButton(action: {
                     store.send(.backButtonTapped)
                 })
+            })
+            .background(.gray50)
+            .overlay(alignment: .center, content: {
+                if store.isShowLogoutAlert {
+                    DDAlert(
+                        title: "로그아웃 하시겠습니까?",
+                        cancelButtonTitle: "취소",
+                        confirmButtonTitle: "확인",
+                        onCancel: {
+                            store.send(.hideLogoutAlert)
+                        },
+                        onConfirm: {
+                            store.send(.logoutButtonTapped)
+                        }
+                    )
+                }
             })
         }
     }
