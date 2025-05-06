87cc61ae9b668cfaacbe318ea9d8ec7e9e66efcf
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-05-06 15:29:30 +0900

    [FEAT] #99 - 화면 추가

---
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/Settings/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  dnd-12th-2-iOS
+//
+//  Created by 권석기 on 5/6/25.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
