e31f9c6cc880f3012e13e4fc0bd0e638b79c82aa
Seokki-Kwon <kwons3327@gmail.com>
2025-05-12 13:36:48 +0900

    [ADD] #101 - TodoDetailView 추가

---
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/Main/Todo/TodoDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  TodoDetailView.swift
+//  dnd-12th-2-iOS
+//
+//  Created by 권석기 on 5/12/25.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+
+struct TodoDetailView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    TodoDetailView()
+}
