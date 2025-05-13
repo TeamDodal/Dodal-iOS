176d0579404446942c70fca8da78640662e3e677
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-05-13 14:43:52 +0900

    [CHORE] #101 - Todo관련 기능 폴더 분리

---
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/Main/Todo/TodoList/TodoListFeature.swift
@@ -0,0 +1,67 @@
+//
+//  TodoListFeature.swift
+//  dnd-12th-2-iOS
+//
+//  Created by 권석기 on 5/10/25.
+//
+
+import ComposableArchitecture
+
+@Reducer
+struct TodoListFeature {
+    @ObservableState
+    struct State {
+        var todoItems: [TodoItem] = []
+    }
+    
+    enum Action: TCAAction {
+        case view(ViewAction)
+        case external(ExternalAction)
+        case destination(DestinationAction)
+        
+        enum ViewAction {
+            case viewonAppear
+            case responseTodoItem([TodoItem])
+            case todoCellTapped(TodoItem)
+        }
+        
+        enum ExternalAction {
+            case fetchTodoItem
+        }
+        
+        enum DestinationAction {}
+    }
+    
+    @Dependency(\.todoClient) var todoClient
+    
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+                // MARK: - view
+            case let .view(viewAction):
+                switch viewAction {
+                case .viewonAppear:
+                    return .run { send in
+                        await send(.external(.fetchTodoItem))
+                    }
+                case let .responseTodoItem(todoItem):
+                    state.todoItems = todoItem
+                    return .none
+                default:
+                    return .none
+                }
+                // MARK: - external
+            case let .external(externalAction):
+                switch externalAction {
+                case .fetchTodoItem:
+                    return .run { send in
+                        let response = try todoClient.fetchTodoItems()
+                        await send(.view(.responseTodoItem(response)))
+                    }
+                }
+            default:
+                return .none
+            }
+        }
+    }
+}
