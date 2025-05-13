535c90dbb2a1e2ba4ea0475ccf8b47f79db84af2
Seokki-Kwon <kwons3327@gmail.com>
2025-05-13 15:30:10 +0900

    [REFACTOR] #101 - TabBar 컴포넌트로 분리

---
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/MainTab/TabBarView.swift
@@ -0,0 +1,42 @@
+//
+//  TabBarView.swift
+//  dnd-12th-2-iOS
+//
+//  Created by 권석기 on 5/13/25.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    let currentTab: TabInfo
+    var body: some View {
+        HStack {
+            ForEach(TabInfo.allCases, id: \.self) { tabInfo in
+                HStack {
+                    HStack {
+                        Spacer()
+                        VStack {
+                            // TODO: 머지하고 이미지 추가예정
+                            Image(tabInfo == currentTab ? tabInfo.iconSelected : tabInfo.icon)
+                            Text(tabInfo.rawValue)
+                                .font(.pretendard(size: 10, weight: .regular))
+                                .foregroundStyle(currentTab == tabInfo ? .blue : .black)
+                        }
+                        Spacer()
+                    }
+                    .padding(.top, 12)
+                    .padding(.bottom, 4)
+                }
+            }
+        }
+        .overlay(alignment: .top) {
+            Divider()
+        }
+        .background(.white)
+        .allowsHitTesting(false)
+    }
+}
+
+//#Preview {
+//    TabBarView()
+//}
