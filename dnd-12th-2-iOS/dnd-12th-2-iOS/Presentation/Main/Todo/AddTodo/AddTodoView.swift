539bbaa723aaa8478687ffca63d6abc7bf587c3b
Seokki-Kwon <kwons3327@gmail.com>
2025-06-01 00:19:15 +0900

    [FEAT] #111 - Calendar 연결

--- dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/Main/Todo/AddTodo/AddTodoView.swift
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/Main/Todo/AddTodo/AddTodoView.swift
@@ -22,52 +22,19 @@ struct AddTodoView: View {
     
     var body: some View {
         WithPerceptionTracking {
-            VStack {
-                TextField("eg.운동하기", text: $store.title)
-                    .font(.pretendard(size: 22, weight: .semibold))
-                    .foregroundStyle(.black)
-                    .padding(.top, 12)
-                    .padding(.horizontal, 16)
-                    .focused($focusedField, equals: .title)
-                TextEditor(text: $store.content)
-                    .frame(height:64)
-                    .overlay(alignment: .topLeading) {
-                        Text("설명")
-                            .font(.pretendard(size: 14, weight: .regular))
-                            .foregroundStyle(.gray400)
-                            .offset(x: 2, y: 8)
-                            .opacity(store.content.isEmpty ? 1 : 0)
-                    }
-                    .padding(.leading, 16)
-                HStack {
-                    endDateButton
-                    Spacer()
-                    createTodoButton
-                }
-                .padding(.vertical, 12)
-                .padding(.horizontal, 16)
-                .overlay(alignment: .top, content: {
-                    Divider()
-                })
-                Spacer()
-            }            
-            .background(.white)
-            .clipShape(
-                .rect(
-                    topLeadingRadius: 12,
-                    bottomLeadingRadius: 0,
-                    bottomTrailingRadius: 0,
-                    topTrailingRadius: 12
-                )
-            )
-            .onAppear {
-                focusedField = .title
+            switch store.viewFlow {
+            case .addTodo:
+                addTodoView
+            case .calendar:
+                calendarView
             }
         }
     }
     
     private var endDateButton: some View {
-        Button(action: {}, label: {
+        Button(action: {
+            store.send(.view(.setDueDateButtonTapped))
+        }, label: {
             HStack(spacing: 4) {
                 Image(.iconCalendarGray)
                 Text("마감일")
@@ -95,6 +62,86 @@ struct AddTodoView: View {
                 .background(.mainBlue)
                 .cornerRadius(8)
         })
+    }
+    
+    private var addTodoView: some View {
+        VStack {            
+            TextField("eg.운동하기", text: $store.title)
+                .font(.pretendard(size: 22, weight: .semibold))
+                .foregroundStyle(.black)
+                .padding(.top, 12)
+                .padding(.horizontal, 16)
+                .focused($focusedField, equals: .title)
+            TextEditor(text: $store.content)
+                .frame(height:64)
+                .overlay(alignment: .topLeading) {
+                    Text("설명")
+                        .font(.pretendard(size: 14, weight: .regular))
+                        .foregroundStyle(.gray400)
+                        .offset(x: 2, y: 8)
+                        .opacity(store.content.isEmpty ? 1 : 0)
+                }
+                .padding(.leading, 16)
+            HStack {
+                endDateButton
+                Spacer()
+                createTodoButton
+            }
+            .padding(.vertical, 12)
+            .padding(.horizontal, 16)
+            .overlay(alignment: .top, content: {
+                Divider()
+            })
+            Spacer()
+        }
+        .background(.white)
+        .clipShape(
+            .rect(
+                topLeadingRadius: 12,
+                bottomLeadingRadius: 0,
+                bottomTrailingRadius: 0,
+                topTrailingRadius: 12
+            )
+        )
+        .onAppear {
+            focusedField = .title
+        }
+    }
+    
+    private var calendarView: some View {
+        VStack {
+            HStack {
+                Button {
+                    store.send(.view(.backButtonTapped))
+                } label: {
+                    Image(.iconBack)
+                }
+                Spacer()
+                Text("마감일")
+                    .font(.pretendard(size: 18, weight: .semibold))
+                Spacer()
+                Image(.iconBack)
+                    .opacity(0)
+            }
+            .frame(height: 32)
+            .padding(.horizontal, 12)
+            .padding(.top, 12)
+            
+            DDCalendar(month: Date(), selectedDate: $store.selectedDate)
+            DDButton(type: .primary, title: "마감일 설정", action: {
+                store.send(.view(.backButtonTapped))
+            })
+                .padding(.horizontal, 16)
+        }
+        .background(.white)
+        .clipShape(
+            .rect(
+                topLeadingRadius: 12,
+                bottomLeadingRadius: 0,
+                bottomTrailingRadius: 0,
+                topTrailingRadius: 12
+            )
+        )
     }
 }
 
