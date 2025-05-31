7984c24022251356c721fd799a63d3c3e886fdfa
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-06-01 05:36:57 +0900

    [REFACTOR] #113 - View에 종속적인 기능을 MainViewFeautre로 이동

--- dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/Main/MainViewFeature.swift
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/Main/MainViewFeature.swift
@@ -4,17 +4,48 @@
 //
 //  Created by 권석기 on 5/10/25.
 //
-
+import Foundation
 import ComposableArchitecture
 
 @Reducer
 struct MainViewFeature {
     @ObservableState
     struct State {
-        var isShowAddTodoSheet = false
+        /// 할일 추가
         var todo = TodoFeature.State(isEdit: false)
+        /// 할일 목록
         var todoList = TodoListFeature.State()
-        var todoTap = TodoTabFeature.State()
+        
+        /// 날짜 설정 관련 객체
+        private let calendar = Calendar.current
+        
+        /// 할일 추가 보임 여부
+        var isShowAddTodoSheet = false
+        /// 마감일 하루전 팝업 여부
+        var isShowDdayPopup = true
+        /// 마감일 하루전 할일 목록
+        var dDayTodos: [Todo] {
+            return todoList.todoItems.filter { todo in
+                guard let dueDate = todo.dueDate else { return false }
+                return calendar.isDateInTomorrow(dueDate)
+            }
+        }
+        /// 이번주 할일 목록
+        var thisWeekTodos: [Todo] {
+            guard let weekInterval = calendar.dateInterval(of: .weekOfYear, for: Date()) else {
+                return []
+            }
+            return todoList.todoItems.filter { todo in
+                guard let dueDate = todo.dueDate else { return false }
+                return weekInterval.contains(dueDate)
+            }.sorted { $0.dueDate ?? $0.createDate < $1.dueDate ?? $1.createDate }
+        }
+        
+        /// 최근 할일 목록
+        /// updateDate 기준으로 최신순 정렬
+        var recentTodos: [Todo] {
+            return todoList.todoItems.sorted { $0.updateDate > $1.updateDate }
+        }
     }
     
     enum Action: ViewAction, TCAAction {
@@ -26,11 +57,17 @@ struct MainViewFeature {
         // SubFeature
         case todo(TodoFeature.Action)
         case todoList(TodoListFeature.Action)
-        case todoTap(TodoTabFeature.Action)
         
         enum ViewAction: BindableAction {
             case binding(BindingAction<State>)
+            /// 할일 목록 추가를 보여줌
             case showAddTodoButtonTapped
+            /// 뷰가 나타났을때 할일 목록을 불러오기
+            case viewOnAppear
+            /// 할일 목록을 탭하는 경우
+            case todoCellTapped(Todo)
+            /// 마감 하루전 팝업 닫기
+            case dismissDdayPopup
         }
         
         enum DestinationAction {
@@ -48,9 +85,6 @@ struct MainViewFeature {
         Scope(state: \.todoList, action: \.todoList) {
             TodoListFeature()
         }
-        Scope(state: \.todoTap, action: \.todoTap) {
-            TodoTabFeature()
-        }
         Reduce { state, action in
             switch action {
             case .view(.binding(\.isShowAddTodoSheet)):
@@ -64,29 +98,26 @@ struct MainViewFeature {
                 case .showAddTodoButtonTapped:
                     state.isShowAddTodoSheet = true
                     return .none
+                case .viewOnAppear:
+                    return .send(.todoList(.view(.viewonAppear)))
+                case let .todoCellTapped(todo):
+                    return .send(.destination(.goToTodoDetail(todo)))
+                case .dismissDdayPopup:
+                    state.isShowDdayPopup = false
+                    return .none
                 }
                 // MARK: - TodoView
             case let .todo(todoAction):
                 switch todoAction {
-               
                 case .view(.addTodoComplete):
                     state.isShowAddTodoSheet = false
-                    return .send(.todoList(.view(.viewonAppear)))                    
+                    return .send(.todoList(.view(.viewonAppear)))
                 default:
                     return .none
                 }
                 // MARK: - TodoListView
             case let .todoList(todoListAction):
-                switch todoListAction {                
-                default:
-                    return .none
-                }
-                
-                // MARK: - TodoTap
-            case let .todoTap(todoTapAction):
-                switch todoTapAction {
-                case let .view(.todoRowTapped(todo)):
-                    return .send(.destination(.goToTodoDetail(todo)))
+                switch todoListAction {
                 default:
                     return .none
                 }
--- dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/Main/Todo/TodoList/TodoListFeature.swift
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/Main/Todo/TodoList/TodoListFeature.swift
@@ -13,36 +13,18 @@ import ComposableArchitecture
 struct TodoListFeature {
         
     @ObservableState
-    struct State {
-        private let calendar = Calendar.current
+    struct State {   
         /// 투두리스트
         var todoItems: [Todo] = []
+        /// 특정 id 하위의 할일 목록만 가져오기 위함
+        var parentID: UUID?
         
-        /// 마감일 하루전 팝업 여부
-        var isShowDdayPopup = false
-        
-        /// 마감일 하루전 할일 목록
-        var dDayTodos: [Todo] {
-            return todoItems.filter { todo in
-                guard let dueDate = todo.dueDate else { return false }
-                return calendar.isDateInTomorrow(dueDate)
-            }
-        }
-        /// 이번주 할일 목록
-        var thisWeekTodos: [Todo] {
-            guard let weekInterval = calendar.dateInterval(of: .weekOfYear, for: Date()) else {
-                return []
-            }
-            return todoItems.filter { todo in
-                guard let dueDate = todo.dueDate else { return false }
-                return weekInterval.contains(dueDate)
-            }.sorted { $0.dueDate ?? $0.createDate < $1.dueDate ?? $1.createDate }
+        init() {
+            self.parentID = nil
         }
         
-        /// 최근 할일 목록
-        /// updateDate 기준으로 최신순 정렬
-        var recentTodos: [Todo] {
-            return todoItems.sorted { $0.updateDate > $1.updateDate }
+        init (parentID: UUID?) {
+            self.parentID = parentID
         }
     }
     
@@ -55,12 +37,9 @@ struct TodoListFeature {
         enum ViewAction {
             /// 뷰가 나타났을때 액션
             case viewonAppear
+            
             /// 할일 목록 받아왔을때
             case responseTodoItem([Todo])
-            /// 마감일 하루전 팝업 닫기
-            case dismissDdayPopup
-            /// 마감일 하루전 팝업 열기
-            case showDdayPopup
             /// 할일 목록 탭했을때
             case todoCellTapped(Todo)
         }
@@ -71,8 +50,7 @@ struct TodoListFeature {
         }
         
         enum DestinationAction {
-            /// 할일 목록 상세로 이동
-            case goToTodoDetailView(Todo)
+   
         }
     }
     
@@ -90,18 +68,8 @@ struct TodoListFeature {
                     }
                 case let .responseTodoItem(todoItem):
                     state.todoItems = todoItem
-                    return .run { send in
-                        try await Task.sleep(for: .seconds(0.5))
-                        await send(.view(.showDdayPopup), animation: .easeInOut)
-                        }
-                case .showDdayPopup:
-                    state.isShowDdayPopup = true
+                default:
                     return .none
-                case .dismissDdayPopup:
-                    state.isShowDdayPopup = false
-                    return .none
-                case let .todoCellTapped(todo):
-                    return .send(.destination(.goToTodoDetailView(todo)))
                 }
                 // MARK: - external
             case let .external(externalAction):
@@ -115,6 +83,7 @@ struct TodoListFeature {
             default:
                 return .none
             }
+            return .none
         }
     }
 }
