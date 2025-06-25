b91d82928920a8da975129da781333e0b55b5cfa
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-06-25 23:49:32 +0900

    [REFACTOR] #6 - 폴더링 수정

---
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/TodoSheet/DueDateCalendarView.swift
@@ -0,0 +1,45 @@
+//
+//  DueDateView.swift
+//  dnd-12th-2-iOS
+//
+//  Created by 권석기 on 6/25/25.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct DueDateCalendarView: View {
+    @Perception.Bindable var store: StoreOf<DueDateCalendarFeature>
+    var body: some View {
+        WithPerceptionTracking {
+            VStack {
+                HStack {
+                    Button(action: {
+                        store.send(.backButtonTapped)
+                    }) {
+                        Image(.iconBack)
+                    }
+                    Spacer()
+                    Text("마감일")
+                        .font(.pretendard(size: 18, weight: .semibold))
+                    Spacer()
+                    Image(.iconBack)
+                        .opacity(0)
+                }
+                .frame(height: 32)
+                .padding(.top, 12)
+                
+                DDCalendar(selectedDate: $store.dueDate)
+                
+                DDButton(type: store.dueDate == nil ? .disabled : .primary, title: store.buttonTitle) {
+                    store.send(.backButtonTapped)
+                }
+            }
+            .padding(.horizontal, 16)
+        }
+    }
+}
+
+//#Preview {
+//    DueDateView()
+//}
