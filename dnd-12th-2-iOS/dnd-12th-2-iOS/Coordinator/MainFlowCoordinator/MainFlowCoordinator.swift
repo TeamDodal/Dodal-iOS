c448f3d9536fd17b0d27772ea4533f8fd2ed6e46
Seokki-Kwon <kwons3327@gmail.com>
2025-06-27 01:05:09 +0900

    [DOCS] #6 - Coordinator 관련 로직 주석 작성

--- dnd-12th-2-iOS/dnd-12th-2-iOS/Coordinator/MainFlowCoordinator/MainFlowCoordinator.swift
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Coordinator/MainFlowCoordinator/MainFlowCoordinator.swift
@@ -16,36 +16,44 @@ struct MainFlowCoordinator {
     
     @ObservableState
     struct State {
+        /// 네비게이션 스택
         var path = StackState<Path.State>()
-        var homeViewState = HomeViewFeature.State()
+        
+        /// HomeView store
+        var homeViewStore = HomeViewFeature.State()
     }
     
     enum Action {
+        /// 네비게이션 스택 액션
         case path(StackActionOf<Path>)
-        case homeViewAction(HomeViewFeature.Action)
+        
+        /// HomeView action
+        case homeViewStore(HomeViewFeature.Action)
     }
     
     var body: some Reducer<State, Action> {
-        Scope(state: \.homeViewState, action: \.homeViewAction) {
+        Scope(state: \.homeViewStore, action: \.homeViewStore) {
             HomeViewFeature()
         }
         Reduce { state, action in
             switch action {
-            case let .homeViewAction(action):
+                // MARK: - homeView action
+            case let .homeViewStore(action):
                 switch action {
+                    // MARK: - View action
                 case let .view(action):
                     switch action {
                     case let .todoCellTapped(todo):
                         state.path.append(.todoDetail(.init(todoItem: todo)))
                         return .none
-                    default:
-                        return .none
+                    default: return .none
                     }
-                default:
-                    return .none
+                default: return .none
                 }
+                // MARK: - Navigation
             case let .path(action):
                 switch action {
+                    // todoDetail
                 case let .element(id: id, action: .todoDetail(.destination(.popNavigationStack))):
                     state.path.pop(from: id)
                     return .none
