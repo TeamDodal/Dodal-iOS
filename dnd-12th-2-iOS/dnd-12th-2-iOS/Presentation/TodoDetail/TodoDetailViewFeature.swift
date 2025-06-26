6bcccd9d34ffc8a07453b361affdeeb7fdc141a4
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-06-27 01:40:32 +0900

    [DOCS] #6 - TodoDetaill 관련 주석 추가

--- dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/TodoDetail/TodoDetailViewFeature.swift
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/TodoDetail/TodoDetailViewFeature.swift
@@ -13,34 +13,41 @@ import ComposableArchitecture
 struct TodoDetailViewFeature {
     @ObservableState
     struct State {
+        /// 상단에 나타나는  todo
         var todoItem: Todo
+        /// todoSheet 보임 여부
         var isShowAddTodoSheet = false
+        /// 삭제 popup 보임 여부
         var isShowDeleteAlert = false
-        var todoSheetState: TodoSheetFeature.State
+        /// todoSheet
+        var todoSheetStore: TodoSheetFeature.State
+        /// todoList
         var todoList: TodoListFeature.State
-        
+        /// todo 추가 가능 여부
         var isOverDepthLimit: Bool {
             todoItem.depth > 3
         }
-        
+        /// 마감일 설정 텍스트
         var dueDateButtonTitle: String {
             todoItem.dueDate?.toMonthDayString ?? "마감일"
         }
-        
+        /// 할일 상세정보를 보여주고 해당 todo의 하위 할일 목록을 보여줌
+        /// - Parameter todoItem: 조회 하고싶은 todo 정보
         init(todoItem: Todo) {
             self.todoItem = todoItem
-            self.todoSheetState = .addSubTodo(parentId: todoItem.id)
+            self.todoSheetStore = .addSubTodo(parentId: todoItem.id)
             self.todoList = .init(parentID: todoItem.id)
         }
     }
     
     enum Action: ViewAction, TCAAction {
-        case todoSheetAction(TodoSheetFeature.Action)
+        /// 할일 생성 bottomSheet에서 일어나는 액션 정의
+        case todoSheetStore(TodoSheetFeature.Action)
+        /// 할일 목록에서 일어나는 액션 정의
         case todoList(TodoListFeature.Action)
         
         // view에서 일어나는 액션을 정의합니다.
         case view(ViewAction)
-        
         // 외부의존성과 일어나는 액션을 정의합니다.
         case external(ExternalAction)
         
@@ -48,26 +55,39 @@ struct TodoDetailViewFeature {
         case destination(DestinationAction)
         
         enum ViewAction: BindableAction {
+            /// 동적으로 바인딩 하기위한 액션
             case binding(BindingAction<State>)
+            /// todoCell 터치하는 경우
             case todoCellTapped(Todo)
+            /// todoCell의 마감일 설정 버튼 탭
             case dueDateButtonTapped(Todo)
+            /// view가 나타났을때 액션
             case viewOnAppear
+            /// 뒤로가기 버튼 탭
             case backButtonTapped
+            /// 완료하기 버튼 탭
             case completeButtonTapped
+            /// 하위 하일 추가 버튼탭
             case createTodoButtonTapped
+            /// todoSheet 백그라운드 터치
             case backgroundTapped
+            /// 삭제하기 버튼 탭
             case deleteButtonTapped
+            /// 삭제하기 popup 활성화
             case showDeleteAlert
+            /// 삭제하기 popup 비활성화
             case showDeleteAlertDismissed
+            /// 수정하기 버튼 탭
             case editButtonTapped
         }
         
         enum DestinationAction {
+            /// 현재 화면 제거 요청
             case popNavigationStack
         }
         
         enum ExternalAction {
-            case deleteTodoItem(id: UUID)            
+            case deleteTodoItem(id: UUID)
             case completeTodoItem(Todo)
         }
     }
@@ -76,7 +96,7 @@ struct TodoDetailViewFeature {
     
     var body: some Reducer<State, Action> {
         BindingReducer(action: \.view)
-        Scope(state: \.todoSheetState, action: \.todoSheetAction) {
+        Scope(state: \.todoSheetStore, action: \.todoSheetStore) {
             TodoSheetFeature()
         }
         Scope(state: \.todoList, action: \.todoList) {
@@ -96,17 +116,20 @@ struct TodoDetailViewFeature {
                     state.todoItem.isCompleted = true
                     return .send(.external(.completeTodoItem(state.todoItem)))
                 case .createTodoButtonTapped:
-                    state.todoSheetState = .init(parentId: state.todoItem.id)
+                    state.todoSheetStore = .init(parentId: state.todoItem.id)
                     state.isShowAddTodoSheet = true
                     return .none
+                    // 백그라운드 터치시 bottomSheet를 제거하거나 편집중인 상태를 변경한다.
+                    // todo 추가 => 편집계속, 삭제 버튼 활성화
+                    // 마감일 설정 => bottomSheet 제거
                 case .backgroundTapped:
-                    guard let currentView = state.todoSheetState.currentView else {
+                    guard let currentView = state.todoSheetStore.currentView else {
                         return .none
                     }
                     if currentView == .editTodo {
-                        state.todoSheetState.todoState.isEditing = false
+                        state.todoSheetStore.todoState.isEditing = false
                     } else {
-                        state.todoSheetState = .init()
+                        state.todoSheetStore = .init()
                         state.isShowAddTodoSheet = false
                     }
                     return .none
@@ -119,23 +142,25 @@ struct TodoDetailViewFeature {
                     state.isShowDeleteAlert = false
                     return .none
                 case .editButtonTapped:
-                    state.todoSheetState = .editTodo(todo: state.todoItem)
+                    state.todoSheetStore = .editTodo(todo: state.todoItem)
                     state.isShowAddTodoSheet = true
                     return .none
                 case let .dueDateButtonTapped(todo):
-                    state.todoSheetState = .setDueDate(todo: todo)
+                    state.todoSheetStore = .setDueDate(todo: todo)
                     state.isShowAddTodoSheet = true
                     return .none
                 default:
                     return .none
                 }
                 // MARK: - CreateTodo action
-            case let .todoSheetAction(action):
+            case let .todoSheetStore(action):
                 switch action {
+                    // todo 추가 완료시 bottomSheet 제거하기
                 case .editingCanelled, .crateTodoCompleted:
                     state.isShowAddTodoSheet = false
-                    state.todoSheetState = .init(parentId: state.todoItem.id)
+                    state.todoSheetStore = .init(parentId: state.todoItem.id)
                     return .send(.todoList(.view(.viewonAppear)))
+                    // todo 수정 완료시 즉시반영
                 case let .todoAction(.editTodoCompleted(updatedTodo)):
                     state.todoItem = updatedTodo
                     return .none
