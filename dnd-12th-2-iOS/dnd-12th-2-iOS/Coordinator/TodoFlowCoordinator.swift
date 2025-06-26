29d8ede2f87d4d21edf72f0b38a069ea2357234c
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-06-27 00:36:33 +0900

    [DELETE] #6 - 기존 TodoFeature 제거

--- dnd-12th-2-iOS/dnd-12th-2-iOS/Coordinator/TodoFlowCoordinator.swift
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Coordinator/TodoFlowCoordinator.swift
@@ -13,28 +13,40 @@ struct TodoFlowCoordinator {
     enum Path {
         case todoDetail(TodoDetailViewFeature)
     }
-
+    
     @ObservableState
     struct State {
         var root = TodoListFeature.State()
         var path = StackState<Path.State>()
     }
-
+    
     enum Action {
         case root(TodoListFeature.Action)
         case path(StackActionOf<Path>)
     }
-
+    
     var body: some Reducer<State, Action> {
         Scope(state: \.root, action: \.root) {
             TodoListFeature()
         }
-
+        
         Reduce { state, action in
             switch action {
             case let .root(.view(.todoCellTapped(todo))):
                 state.path.append(.todoDetail(.init(todoItem: todo)))
-                return .none                        
+                return .none
+            case let .path(action):
+                switch action {
+                case let .element(id: id, action: .todoDetail(.destination(.popNavigationStack))):
+                    state.path.pop(from: id)
+                    return .none
+                case let .element(id: _, action: .todoDetail(.view(.todoCellTapped(todo)))):
+                    state.path.append(.todoDetail(.init(todoItem: todo)))
+                    return .none
+                default:
+                    return .none
+                }
+                
             default:
                 return .none
             }
