7a6d7f69aa70b392aa75b625afdfae43b0f7a193
Allie <wooyani77@gmail.com>
2025-07-08 21:42:42 +0900

    [FIX] #14 - Onboarding keyboard 가림 현상 수정

--- dnd-12th-2-iOS/dnd-12th-2-iOS/Coordinator/AppFlowCoordinator/AppFlowCoordinatorView.swift
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Coordinator/AppFlowCoordinator/AppFlowCoordinatorView.swift
@@ -24,6 +24,7 @@ struct AppFlowCoordinatorView: View {
                 case .onboarding:
                     CaseLet(/AppFlowCoordinator.State.onboarding, action: AppFlowCoordinator.Action.onboarding) {
                         OnboardingView(store: $0)
+                            .ignoresSafeArea(.keyboard, edges: .bottom)
                     }
                 }
             }
